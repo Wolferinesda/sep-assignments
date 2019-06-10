4ab876b7b9a3f0a1cf089c76ef45b48ef7ab828e
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-06-10 19:03:44 -0400

    Error with resizing

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -1,30 +1,93 @@
 require_relative 'node'
 
 class OpenAddressing
+  attr_accessor :items
+
   def initialize(size)
+    @items = Array.new(size)
+    @total_nodes = 0
   end
 
   def []=(key, value)
+    i = index(key, size)
+    new_node = Node.new(key, value)
+
+    if @items[i].nil?
+      @items[i] = new_node
+    elsif @items[i].key != key
+      self.resize
+      return self[key] = value
+    elsif @items[i].key == key && @items[i].value != value
+      new_index = next_open_index(i)
+      if new_index == -1
+        puts "array full resizing"
+        self.resize
+        return self[key] = value
+      else
+        @items[new_index] = new_node
+      end
+    end
+    @total_nodes += 1
   end
 
   def [](key)
+    i = index(key, self.size)
+
+    if !@items[i].nil? && @items[i].key == key
+      @items[i].value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
+  end
+
+  def load_factor
+    @total_nodes.to_f/ self.size.to_f
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    while index < @items.length
+      if @items[index].nil?
+        return index
+      else
+        index += 1
+      end
+    end
+    return -1
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.count
   end
 
   # Resize the hash
   def resize
+    temp_array = @items
+    @items = Array.new(self.size * 2)
+    @total_nodes = 0
+
+    temp_array.each do |item|
+      if item != nil
+        self[item.key] = item.value
+      end
+    end
+  end
+
+  #Prints out the items' index, key, and value plus the total number of nodes plays the load factor
+  def print_info
+    @items.each_with_index do |item, index|
+      if !item.nil?
+        puts "Hash Index: " + index.to_s
+        puts "Key: " + item.key + ", " + "Value: " + item.value
+      end
+    end
+    puts "Total_Nodes: " + @total_nodes.to_s
+    puts "Load_Factor: " + self.load_factor.to_s
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -4,30 +4,84 @@ class SeparateChaining
   attr_reader :max_load_factor
 
   def initialize(size)
+    @items = Array.new(size)
     @max_load_factor = 0.7
+    @total_nodes = 0
   end
 
   def []=(key, value)
+    i = index(key, self.size)
+    new_node = Node.new(key, value)
+
+    if @items[i].nil?
+      @items[i] = LinkedList.new
+    end
+
+    @items[i].add_to_tail(new_node)
+    @total_nodes += 1
+
+    if self.load_factor > @max_load_factor
+      resize
+    end
   end
 
   def [](key)
+    i = index(key, self.size)
+    @head = @items[i].head
+
+    until @head.nil?
+        return @head.value if @head.key == key
+        @head = @head.next
+    end
+    return
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @total_nodes.to_f/ self.size.to_f
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.count
   end
 
   # Resize the hash
   def resize
+    temp_array = Array.new(@items.size * 2)
+
+    @items.each do |item|
+      if !item.nil?
+        i = index(item.head.key, temp_array.size)
+
+        temp_array[i] = LinkedList.new if temp_array[i].nil?
+
+        current_item = item.head
+
+        while current_item
+          temp_array[i].add_to_tail(current_item)
+          current_item = current_item.next
+        end
+      end
+      @items = temp_array
+    end
+  end
+
+  def print
+    @items.each_with_index do |list, index|
+      if !list.nil?
+        puts "Hash Index: " + index.to_s
+        list.print
+      end
+    end
+    puts "Total_Nodes: " + @total_nodes.to_s
+    puts "Load_Factor: " + self.load_factor.to_s
   end
 end
